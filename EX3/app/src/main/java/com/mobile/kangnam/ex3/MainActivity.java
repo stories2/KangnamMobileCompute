6049a9ae094a93082f9ec96caff83c6ed9c1429a
Kim HyunWoo <stories2@naver.com>
2017-10-10 13:20:46 +0900

    EX3 module : getting error msg from server

--- EX3/app/src/main/java/com/mobile/kangnam/ex3/MainActivity.java
+++ EX3/app/src/main/java/com/mobile/kangnam/ex3/MainActivity.java
@@ -8,6 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends Activity implements View.OnClickListener{
 
     final String TAG = "EX3";
@@ -55,8 +63,39 @@ public class MainActivity extends Activity implements View.OnClickListener{
             @Override
             protected String doInBackground(String... params) {
                 Log.d(TAG, "getting data url: " + params[0]);
+                try {
+                    URL url = new URL(params[0]);
+
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+
+                    InputStream inputStream = httpURLConnection.getInputStream();
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String htmlSource = "", temp = "";
+                    while((temp = bufferedReader.readLine()) != null) {
+                        htmlSource = htmlSource + temp;
+                    }
+                    return htmlSource;
+                }
+                catch (Exception err) {
+                    Log.d(TAG, "Error in doinBackground: " + err.getMessage());
+                }
                 return "Ok";
             }
-        }.execute("http://i2max-ml.xyz:8080");
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                try {
+                    Log.d(TAG, "Response data: " + s);
+                    JSONObject jsonObject = new JSONObject(s);
+                    etxtResult.setText(jsonObject.getString("msg"));
+                }
+                catch (Exception err) {
+                    Log.d(TAG, "Error in on post execute: " + err.getMessage());
+                }
+            }
+        }.execute("http://i2max-ml.xyz:8080/openapi.jsp");
     }
 }
