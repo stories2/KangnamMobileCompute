724abf22eddfb3d974626318b211008709f8f092
Kim HyunWoo <stories2@naver.com>
2017-10-10 12:57:18 +0900

    EX3 module : setup button event

--- EX3/app/src/main/java/com/mobile/kangnam/ex3/MainActivity.java
+++ EX3/app/src/main/java/com/mobile/kangnam/ex3/MainActivity.java
@@ -4,15 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
     final String TAG = "EX3";
 
+    EditText etxtName, etxtResult;
+    Button btnAdd, btnList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init();
+    }
+
+    public void Init() {
+        etxtName = (EditText) findViewById(R.id.etxtName);
+        etxtResult = (EditText)findViewById(R.id.etxtResult);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnList = (Button)findViewById(R.id.btnList);
+
+        btnAdd.setOnClickListener(this);
+        btnList.setOnClickListener(this);
     }
 
     @Override
