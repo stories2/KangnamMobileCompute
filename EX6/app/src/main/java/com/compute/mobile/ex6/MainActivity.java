bfd8c362eb67236d7bfcea1da332ab785e040e8e
Kim HyunWoo <stories2@naver.com>
2017-10-31 13:57:26 +0900

    EX6 module : make new project

---
+++ EX6/app/src/main/java/com/compute/mobile/ex6/MainActivity.java
@@ -0,0 +1,17 @@
+package com.compute.mobile.ex6;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
